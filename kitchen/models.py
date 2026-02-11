7ac3a8d9804b9b73f75e48febdc50f6d9ed70b7f
VolodymyrHavrylenko <volodymyr.v.havrylenko@gmail.com>
2026-02-11 22:34:36 +0200

    feat: Create Cook model inheriting from AbstractUser with year_of_experience

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Cook(AbstractUser):
+    year_of_experience = models.PositiveSmallIntegerField(
+        default=0,
+        help_text="Number of years of cooking experience",
+    )
+
+    class Meta:
+        ordering = ["-year_of_experience"]
+
+    def __str__(self):
+        return (f"{self.username} ({self.first_name} {self.last_name}) "
+                f"- {self.year_of_experience} years of experience")
