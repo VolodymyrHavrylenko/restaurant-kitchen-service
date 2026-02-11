dddc3f66eace0247b54ef1b21863079e08b836ac
VolodymyrHavrylenko <volodymyr.v.havrylenko@gmail.com>
2026-02-11 22:45:44 +0200

    feat: Create DishType model with unique name and help_text

--- kitchen/models.py
+++ kitchen/models.py
@@ -14,3 +14,19 @@ class Cook(AbstractUser):
     def __str__(self):
         return (f"{self.username} ({self.first_name} {self.last_name}) "
                 f"- {self.year_of_experience} years of experience")
+
+
+class DishType(models.Model):
+    name = models.CharField(
+        max_length=100,
+        unique=True,
+        help_text="Type of dish",
+    )
+
+    class Meta:
+        verbose_name = "Dish Type"
+        verbose_name_plural = "Dish Types"
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
