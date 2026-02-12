8a1e13c3e6f401c85acc93a509331829178edc02
VolodymyrHavrylenko <volodymyr.v.havrylenko@gmail.com>
2026-02-12 13:26:26 +0200

    feat: add kitchen urls, index view, base template, and initial templates structure

--- restaurant_kitchen_service/urls.py
+++ restaurant_kitchen_service/urls.py
@@ -15,9 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from debug_toolbar.toolbar import debug_toolbar_urls
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-] + debug_toolbar_urls()
+    path("", include("kitchen.urls", namespace="kitchen")),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("__debug__/", include("debug_toolbar.urls")),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
