9b873f854a783bbc1515163ada4ade6bd9a3d219
VolodymyrHavrylenko <volodymyr.v.havrylenko@gmail.com>
2026-02-12 14:38:12 +0200

    feat: add base template with includes, sidebar, paginator, home page, login and logout pages

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
 
+from kitchen.models import Cook, Dish, DishType, Ingredient
+
+
 def index(request):
-    pass
+    cooks = Cook.objects.count()
+    dishes = Dish.objects.count()
+    types_of_dishes = DishType.objects.count()
+    ingredients = Ingredient.objects.count()
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+    context = {
+        "cooks": cooks,
+        "dishes": dishes,
+        "types_of_dishes": types_of_dishes,
+        "ingredients": ingredients,
+        "num_visits": request.session["num_visits"],
+    }
+    return render(request, 'kitchen/index.html', context=context)
