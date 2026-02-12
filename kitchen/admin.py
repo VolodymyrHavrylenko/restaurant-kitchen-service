99f0c0461cd309c1d6603d4eee96151a85b1997c
VolodymyrHavrylenko <volodymyr.v.havrylenko@gmail.com>
2026-02-12 12:11:39 +0200

    chore: customize Cook admin, add year_of_experience, and remove Groups from admin

--- kitchen/admin.py
+++ kitchen/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from kitchen.models import (
+    Cook,
+    Dish,
+    DishType,
+    Ingredient
+)
+
+admin.site.unregister(Group)
+@admin.register(Cook)
+class CookAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + (
+        "year_of_experience",
+    )
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Additional info",
+            {"fields": ("year_of_experience",)}
+        ),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            "Additional info",
+            {
+                "fields": (
+                "first_name",
+                "last_name",
+                "year_of_experience",
+            )
+            }
+        ),
+    )
+
+
+
